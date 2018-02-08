88cf6e12782a4721626d49edec1589d5dda38f20
balodi <balodi@gmail.com>
2018-02-08 19:40:57 +0530

    linkedlist- testcase passed-linked list created and elements added

---
+++ linked list/first/createlinkedlist.js
@@ -0,0 +1,13 @@
+const CreateLinkedList = require('./linkedlist');
+
+const linkedList1 = new CreateLinkedList();
+
+linkedList1.insertIntoLinkedList(5);
+linkedList1.insertIntoLinkedList(6);
+
+linkedList1.insertIntoLinkedList(7);
+linkedList1.insertIntoLinkedList(8);
+
+// linkedList1.displayLinkedList();
+
+module.exports = linkedList1;
--- linked list/first/test.js
+++ linked list/first/test.js
@@ -1,12 +1,12 @@
-const LinkedList = require('./createlinkedlist.js');
 const CreateLinkedList = require('./linkedlist.js');
+const linkedList = require('./createlinkedlist.js');
 const Node = require('./node.js');
 
 describe('node', () => {
   test(' file should return function', () => {
     expect(typeof (Node)).toBe('function');
   });
-  test('function return function', () => {
+  test('function node return function', () => {
     expect(typeof (new Node(1))).toBe('object');
   });
 });
@@ -14,7 +14,21 @@ describe('linkedList', () => {
   test(' file should return function', () => {
     expect(typeof (CreateLinkedList)).toBe('function');
   });
-  test('function return function', () => {
+  test('function CreateLinkedList return function', () => {
     expect(typeof (new CreateLinkedList())).toBe('object');
+  });
+});
+describe('CreatelinkedList', () => {
+  test('linked list returns an object', () => {
+    expect(typeof (linkedList)).toBe('object');
+  });
+  test('linked list created and elements added', () => {
+    let tempHead = linkedList.Head;
+    let length = 0;
+    while (tempHead !== null) {
+      length += 1;
+      tempHead = tempHead.next;
+    }
+    expect(length).toBe(4);
   });
 });
